59c884e6e177158b32efd70aee3867bdf2c25e0e
Italo Pires <italonapalm@gmail.com>
2025-06-09 15:18:21 -0300

    Adding feature to read value from both value1 and value2 and concatenate and alternating each character from both values

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -2,9 +2,25 @@ import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
+const merge = (s1, s2) => {
+	// var value = '';
+	// for (var i = 0; i < s1; i++) {
+	// 	value += (s1[i] || '')+(s2[i] || '');
+	// }
+	// return value;
+
+	return [...s1].map((e, i) => `${e}${s2[i] || ''}`).join("")
+}
+
 const UseRef = (props) => {
   	const [value1, setValue1] = useState("");
+  	const [value2, setValue2] = useState("");
+	
 	const count = useRef(0);
+	const myInput1 = useRef(null);
+	const myInput2 = useRef(null);
+
+	console.log(myInput1.current);
 
 	// When every rendering happen, it will increment
 	// useRef wont trigger any rendering of this component when its useRef variable change
@@ -13,7 +29,13 @@ const UseRef = (props) => {
 	// When value1 change
 	useEffect(() => {
 		count.current = count.current + 1;
+		myInput2.current.focus();
 	}, [value1])
+
+	useEffect(() => {
+		count.current = count.current + 1;
+		myInput1.current.focus();
+	}, [value2])
 
     return (
         <div className="UseRef">
@@ -31,8 +53,17 @@ const UseRef = (props) => {
 					<span className="text">]</span>
 				</div>
               <input type="text" className="input" 
+			  	ref={myInput1}
                 value={value1} onChange={(event) => setValue1(event.target.value)} />
             </div>
+
+			<SectionTitle title="Way #02" />
+			<div className="center">
+				<input type="text" className="input" 
+					ref={myInput2}
+					value={value2} onChange={(e) => {setValue2(e.target.value)}} />
+				<span className="text red">{merge(value1, value2)}</span>
+			</div>
         </div>
     )
 }
