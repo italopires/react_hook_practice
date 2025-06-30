e1dd9ffc747e748ac5039244d1cdc2c0ed0d11d9
Italo Pires <italonapalm@gmail.com>
2025-06-30 16:42:08 -0300

    Adding loading component

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,10 +1,23 @@
-import { Header } from "@/components/Header";
+import { postRepository } from '@/repositories/post/json-post-repository';
 
 export default async function Home() {
-  console.log('server')
+  const posts = await postRepository.findAll();
+
   return (
     <div>
-      <Header></Header>
+      <header>
+        <h1 className='text-6x1 font-bold text-center py-8'>
+          This is the Header
+        </h1>
+      </header>
+      {posts.map((post) => {
+        return <p key={post.id}>{post.title}</p>;
+      })}
+      <footer>
+        <footer className='text-6x1 font-bold text-center py-8'>
+          This is the Footer
+        </footer>
+      </footer>
     </div>
   );
 }
--- blog/src/repositories/post/json-post-repository.ts
+++ blog/src/repositories/post/json-post-repository.ts
@@ -1,35 +1,46 @@
-import { PostModel } from "@/models/post";
-import { PostRepository } from "./post-repository";
-import { resolve } from "path";
-import { readFile } from "fs/promises";
+import { PostModel } from '@/models/post';
+import { PostRepository } from './post-repository';
+import { resolve } from 'path';
+import { readFile } from 'fs/promises';
 
 const ROOT_DIR = process.cwd();
 const JSON_POSTS_FILE_PATH = resolve(
   ROOT_DIR,
-  "src",
-  "db",
-  "seed",
-  "posts.json"
+  'src',
+  'db',
+  'seed',
+  'posts.json'
 );
+const SIMULATE_WAIT_IN_MS = 1000;
 
 export class JsonPostRepository implements PostRepository {
+  private async simulateWait() {
+    if (SIMULATE_WAIT_IN_MS <= 0) return;
+
+    await new Promise((resolve) => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
+  }
+
   private async readFromDisk() {
-    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8');
     const { posts } = JSON.parse(jsonContent);
 
     return posts;
   }
 
   async findAll(): Promise<PostModel[]> {
+    await this.simulateWait();
+
     const posts = await this.readFromDisk();
     return posts;
   }
 
   async findById(id: string): Promise<PostModel> {
+    await this.simulateWait();
+
     const posts = await this.readFromDisk();
     const post = posts.find((post) => post.id === id);
 
-    if (!post) throw new Error("Post not found!");
+    if (!post) throw new Error('Post not found!');
 
     return post;
   }
